9d5bf7315473f0ed44fb8a143600054da25e5a44
rearl89 <rogerearl@me.com>
2023-05-12 23:44:28 -0700

    Added another mixin

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,10 +5,10 @@ export default function Navbar() {
     return (
         <div className="navbar">
             <div className="links">
-                <a href="/home">Home</a>
-                <a href="/about">About</a>
-                <a href="/profile">Profile</a>
-                <a href="/contact">Contact</a>
+                <a href="/home" className="homeLink">Home</a>
+                <a href="/about" className="aboutLink">About</a>
+                <a href="/profile" className="profileLink">Profile</a>
+                <a href="/contact" className="contactLink">Contact</a>
             </div>
         </div>
     )
