af86409df77585e495669cabc6553b688ee95083
rearl89 <rogerearl@me.com>
2023-05-13 00:23:31 -0700

    Finished

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -6,6 +6,11 @@ export default function MainPage() {
         <div className="mainPage">
             <h1>Welcome to the site</h1>
             <button className="mainBtn">Click Here</button>
+            <div className="otherLinks">
+                <a href="/movies">Movies</a>
+                <a href="/music">Music</a>
+                <a href="/books">Books</a>
+            </div>
         </div>
     )
 }
